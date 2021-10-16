7d4a4170de230ee309c51de7e3d5fc55ce72c88a
Locura78 <gurrestarazu@hotmail.com>
2021-10-16 01:08:52 -0300

    Renombrando la carpeta build por docs

---
+++ docs/precache-manifest.d735264ac3ef555fbbf80bf393310c19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a287e700f418013aad72e2022dfb87a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b15c2939bb9fc0424e77",
+    "url": "/static/css/main.f9a9f94e.chunk.css"
+  },
+  {
+    "revision": "84a1ed7605156b1efa12",
+    "url": "/static/js/2.ac4044b4.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.ac4044b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15c2939bb9fc0424e77",
+    "url": "/static/js/main.80d7d731.chunk.js"
+  },
+  {
+    "revision": "851e4c3c4aa894498d54",
+    "url": "/static/js/runtime-main.8ac25dc8.js"
+  }
+]);
